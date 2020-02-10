11cf0441795c6c1a91152314a748fc2e53be9bfe
B. Bueno <bbueno25@hotmail.com>
2020-02-10 09:48:10 -0700

    modified bitcoin_trading_with_python.py (Bitcoin Trading - Visualizing our data in Matplotlib)

--- bitcoin_trading_with_python/bitcoin_trading_with_python.py
+++ bitcoin_trading_with_python/bitcoin_trading_with_python.py
@@ -1,1 +1,22 @@
+import datetime
+import time
+import matplotlib
+import numpy
 
+def testing_data():
+    dates, prices, volumes = [], [], []
+    try:
+        source_code = open('btceUSD.csv', 'r').read()
+        split_source = ssource_code.split('\n')
+        for line in split_source[-50000:]:
+            split_line = line.split(',')
+            dates.append(float(split_line[0]))
+            prices.append(float(split_line[1]))
+            volumes.append(float(split_line[2]))
+    except Exception as exception:
+        print('Failed:Raw Data:' + str(exception))
+    matplotlib.pyplot.plot(dates, prices)
+    matplotlib.pyplot.show()
+
+if __name__ == '__main__':
+    testing_data()
